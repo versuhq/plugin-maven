baff3d0f24590ec4796cdbe1388ca3dbed25cc0f
Tiago Santos <tvcsantos@gmail.com>
2026-03-31 12:57:47 +0100

    feat: update core package

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated. Do not edit manually.
 // Run 'npm run generate-version' to update this file.
-export const VERSION = "0.1.4";
+export const VERSION = "0.2.0";
 export const PACKAGE_NAME = "@versu/plugin-maven";
 export const AUTHORS = "tvcsantos";
